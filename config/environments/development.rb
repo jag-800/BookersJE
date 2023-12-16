6c2fd65c7146814b6e667e888fd03a22463da34d
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2023-12-16 16:02:14 +0000

    パスワード変更機能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,16 +77,17 @@ Rails.application.configure do
   config.active_job.queue_adapter = :inline
   
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    port:                 587,
-    address:              'smtp.gmail.com',
-    domain:               'gmail.com',
-    user_name:            ENV['USER_NAME'],
-    password:             ENV['APP_PASS'],
-    authentication:       'login',
-    enable_starttls_auto: true
+      :user_name => ENV['USER_NAME'],
+      :password => ENV['APP_PASS'],
+      :domain => "gmail.com",
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
   }
 
 
